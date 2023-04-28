3feff01cc1b45afe767a0d0e3f8b2970415782fc
Aleksei Falchuk <sddw28@gmail.com>
2023-04-28 15:13:48 +0200

    feat: add title & keyboard area

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ function createComponent(tag, classNam) {
 function createWindow() {
   const mainDiv = createComponent('div', 'main-div');
   document.body.append(mainDiv);
+  const title = createComponent('h1', 'title');
+  title.innerText = 'RSS Virtual keyboard';
+  mainDiv.append(title);
   const textArea = createComponent('textarea', 'text-area');
   textArea.style.backgroundColor = 'rgb(153, 170, 181)';
   mainDiv.append(textArea);
+  const keyboardArea = createComponent('div', 'keyboard');
+  mainDiv.append(keyboardArea);
 }
 
 createWindow();
